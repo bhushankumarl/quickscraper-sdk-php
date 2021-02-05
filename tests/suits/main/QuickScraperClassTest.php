3bd0ef65ddcb68491d5123400ed982eedad2cdeb
Bhushankumar L <bhushankumar.lilapara@gmail.com>
2021-02-05 14:56:50 +0530

    Added new testcase

--- tests/suits/main/QuickScraperClassTest.php
+++ tests/suits/main/QuickScraperClassTest.php
@@ -1,18 +1,25 @@
-<?php 
+<?php
 
 namespace QuickScraper\Main;
 
+use GuzzleHttp\Client;
 use PHPUnit\Framework\TestCase;
 
 /**
-*  Corresponding class to test YourClass class
-*
-*  For each class in your library, there should be a corresponding unit test
-*
-*  @author yourname
-*/
+ *  Corresponding class to test YourClass class
+ *
+ *  For each class in your library, there should be a corresponding unit test
+ *
+ *  @author yourname
+ */
 class QuickScraperClassTest extends TestCase
 {
+    private $accessToken = '';
+    private $parseUrl;
+    private $DEFAULT = array(
+      "CLIENT" =>'NODEJS_CLIENT_LIB',
+      "HOST" => 'https://rest.quickscraper.co/'
+    );
     /**
      * Just check if the YourClass has no syntax errors
      */
@@ -29,7 +36,42 @@ class QuickScraperClassTest extends TestCase
     {
         $this->assertTrue(true, getenv('QS_ACCESS_TOKEN'));
     }
+    /**
+     * Just check if set host is number value
+     */
+    public function testSetHost()
+    {
+        $object = new QuickScraperClass('Dummy');
 
+        $this->assertFalse(false,"Only string allow in set host : ".$object->setHost(213));
+    }
+    
+    /**
+     * Just check if set host is number value
+     */
+    public function testSetAccessTokenNumberType()
+    {
+        $object = new QuickScraperClass('Dummy');
+
+        $this->assertFalse(false,"Only string allow in set setAccessToken : ".$object->setAccessToken(213));
+    }
+    /**
+     * Just check if getting html result
+     */
+    public function testGetHtmlAllowOnlyStringParameter()
+    {
+        $object = new QuickScraperClass(getenv('QS_ACCESS_TOKEN'));
+        $httpClient = new Client();
+        $requestUrl = $this->prepareRequestUrl('https://google.com');
+        $headers = $this->prepareHeaders();
+        $options = array(
+            'headers'=> $headers
+        );
+        $response = $httpClient->getAsync($requestUrl, $options)->wait();
+
+        $this->getStatus(400,$response->getStatusCode());
+    }
+    
     /**
      * Test the only existing method of the class
      *
@@ -45,16 +87,20 @@ class QuickScraperClassTest extends TestCase
         $this->assertTrue($expected, $object->sayHello($name));
     }
 
-   
-    public function testSetAccessToken()
-    {
-        $object = new QuickScraperClass('dummy');
 
-        $this->assertFalse(false, $object->setAccessToken('dummy'));
+    private function prepareRequestUrl(string $url): string
+    {
+        $object = new QuickScraperClass(getenv('QS_ACCESS_TOKEN'));
+
+        $requestUrl = $object->parseUrl.'?access_token='.$this->accessToken.'&URL='.$url;
+        return $requestUrl;
     }
-   
-   
-    
+    private function prepareHeaders() {
+      $headers = array(
+        'client' => $this->DEFAULT['CLIENT']
+      );
+      return $headers;
+    }
     /**
      * Data for sayHello
      *
