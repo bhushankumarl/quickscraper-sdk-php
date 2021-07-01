b85cbb4a3cc69a4206d46b1b61a5f978318618aa
KJG <app@quickscraper.co>
2021-07-01 21:09:46 +0530

    Fix concurrent testcase

--- tests/suits/main/ParseConCurrentTest.php
+++ tests/suits/main/ParseConCurrentTest.php
@@ -1,33 +1,35 @@
 <?php
+
 namespace QuickScraper\Main;
 
 use PHPUnit\Framework\TestCase;
 use QuickScraper\Tests\Suits\Main\MockConfig;
 use QuickScraper\Main\QuickScraperClass;
-use GuzzleHttp\Exception\RequestException;
 
-class ParseConCurrentTest extends TestCase 
+class ParseConCurrentTest extends TestCase
 {
+	/**
+	 * ----------------------------------------------------------------------
+	 * Import : Request should be failed if concurrent request count exceed
+	 * ----------------------------------------------------------------------
+	 */
+	public function testConCurrentRequest()
+	{
+		$QuickScraperClient = new QuickScraperClass(MockConfig::getAccessToken());
+		$requestUrl = MockConfig::sampleRequestUrl();
 
-  /** 
-   * ----------------------------------------------------------------------
-   * Import : Request should be failed if concurrent request count exceed 
-   * ----------------------------------------------------------------------
-   */
-  public function testConCurrentRequest()
-  {
-    $QuickScraperClient = new QuickScraper(MockConfig::getAccessToken());
-    $requestUrl = MockConfig::sampleRequestUrl();
-  
-    try {
-      $promises = [];
-      for ($index = 0; $index < MockConfig::getAccessToken(); $index++) {
-        $promisesGetHtml = $QuickScraperClient->getHtml($requestUrl);
-        array_push($promises,$promisesGetHtml);
-      }
-      $this->assertArrayHasKey('data', $response);
-    } catch (\Exception $error) {
-      $this->expectException($error);
-    }
-  }
+		try {
+			$promises = [];
+			for ($index = 0; $index < MockConfig::getConcurrentCount(); $index++) {
+				$promisesGetHtml = $QuickScraperClient->getHtml($requestUrl);
+				$responseArray = json_decode($promisesGetHtml);
+				array_push($promises, $responseArray);
+				$this->assertObjectHasAttribute('data', $responseArray);
+				$this->assertObjectNotHasAttribute('message', $responseArray);;
+			}
+			$this->assertNotEmpty($promises);
+		} catch (\Exception $error) {
+			$this->expectException($error);
+		}
+	}
 }
