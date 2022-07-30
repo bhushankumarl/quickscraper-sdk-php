735dccf774c66a5d22fd783b7c0edbcb5fa36c26
QS Apps <app@quickscraper.co>
2022-07-30 18:26:42 +0530

    Throw exception

--- src/QuickScraperClass.php
+++ src/QuickScraperClass.php
@@ -74,14 +74,16 @@ class QuickScraperClass
       if ($responseError->message && $responseError->statusCode) {
         $statusCode =  $responseError->statusCode ? $responseError->statusCode : 530;
         http_response_code($statusCode);
-        return json_encode($responseError);
+        // return json_encode($responseError);
+        throw new \Exception($responseError->message, $statusCode);
       }
       $statusCode = 530;
       $message = 'Failed to process request';
       $type = 'UNKNOWN';
       $throwError = array('message' => $message, 'status' => $statusCode);
       http_response_code($statusCode);
-      return json_encode($throwError);
+      // return json_encode($throwError);
+      throw new \Exception($throwError['message'], $statusCode);
     }
   }
   private function post(string $url, array $parseOptions = [])
