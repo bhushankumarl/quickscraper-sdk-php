5c40ead9ab32fa0a2bd8079a74480762be12b0bb
QS Apps <app@quickscraper.co>
2023-08-03 15:07:07 +0530

    fixed response not getting issue for post request

--- src/QuickScraperClass.php
+++ src/QuickScraperClass.php
@@ -55,18 +55,14 @@ class QuickScraperClass
       'headers' => $headers,
       'verify' => false
     );
-   
     try {
       $httpClient = new Client();
-      if (($parseOptions['customSelectors'] && sizeof($parseOptions['customSelectors']) > 0) || $parseOptions['formData']) {
+      if ((isset($parseOptions['customSelectors']) && sizeof($parseOptions['customSelectors']) > 0) || isset($parseOptions['formData'])) {
         $bodyData = $this->prepareRequestBody($url, $parseOptions);
-        echo 'here is echo';
-        echo json_encode($bodyData);
-        $gotOptions['body'] = json_encode($bodyData);
-        $response = $httpClient->postAsync($url, $gotOptions)->wait();
+        $gotOptions['form_params'] = $bodyData;
+        $response = $httpClient->postAsync($this->parseUrl, $gotOptions)->wait();
         return $response;
       }
-      echo 'here is not a post request';
       $requestUrl = $this->prepareRequestUrl($url, $parseOptions);
       $response = $httpClient->getAsync($requestUrl, $gotOptions)->wait();
       $responseObject = (Object) array(
@@ -248,35 +244,8 @@ class QuickScraperClass
     }
   }
 
-  private function postContent($url, $parseOptions, $headers)
+  private function prepareRequestBody($url, $parseOptions)
   {
-    try {
-      $httpClient = new Client();
-      $response = $httpClient->postAsync($url, $parseOptions)->wait();
-      $responseObject = (Object) array(
-        'data' => $response->getBody()->getContents()
-      );
-      return json_encode($responseObject, JSON_HEX_TAG);
-    } catch (RequestException $exception) {
-      $response = $exception->getResponse();
-      $responseError = json_decode((string) $response->getBody());
-      if ($responseError->message && $responseError->statusCode) {
-        $statusCode = $responseError->statusCode ? $responseError->statusCode : 530;
-        http_response_code($statusCode);
-        // return json_encode($responseError);
-        throw new \Exception($responseError->message, $statusCode);
-      }
-      $statusCode = 530;
-      $message = 'Failed to process request';
-      $type = 'UNKNOWN';
-      $throwError = array('message' => $message, 'status' => $statusCode);
-      http_response_code($statusCode);
-      // return json_encode($throwError);
-      throw new \Exception($throwError['message'], $statusCode);
-    }
-  }
-
-  private function prepareRequestBody($url, $parseOptions){
     $urlOptions = array(
       'access_token' => $this->accessToken,
       'URL' => $url
@@ -318,17 +287,17 @@ class QuickScraperClass
       $urlOptions['isZapier'] = $parseOptions['isZapier'];
     }
     if (isset($parseOptions['customSelectors'])) {
-      $urlOptions['customSelectors'] = $parseOptions['customSelectors'];
-		}
-		if (isset($parseOptions['keepSelection'])) {
+      $urlOptions['customSelectors'] = json_encode($parseOptions['customSelectors']);
+    }
+    if (isset($parseOptions['keepSelection'])) {
       $urlOptions['keepSelection'] = $parseOptions['keepSelection'];
-		}
-		if (isset($parseOptions['formData'])) {
-      $urlOptions['formData'] = $parseOptions['formData'];
-		}
-		if (isset($parseOptions['isKeepFormDataSelection'])) {
-      $urlOptions['isKeepFormDataSelection'] = $parseOptions['isKeepFormDataSelection'];
-		}
+    }
+    if (isset($parseOptions['formData'])) {
+      $urlOptions['formData'] = json_encode($parseOptions['formData']);
+    }
+    if (isset($parseOptions['keepFormdataSelection'])) {
+      $urlOptions['keepFormdataSelection'] = $parseOptions['keepFormdataSelection'];
+    }
 
     return $urlOptions;
   }
--- tests/suits/main/ParseConCurrentTest.php
+++ tests/suits/main/ParseConCurrentTest.php
@@ -19,47 +19,16 @@ class ParseConCurrentTest extends TestCase
 		$requestUrl = MockConfig::sampleRequestUrl();
 
 		try {
-			$parseOptions = array(
-				'URL' => 'https://www.amazon.com',
-				'country_code' => 'any',
-				'customSelectors' => array(
-					array(
-						'name' => 'waitForSelector',
-						'selectorScript' => '#search',
-						'options' => null,
-						'isScript' => false
-					),
-				),
-				'keepSelection' => true,
-				'formData' => array(
-					'formSelector' => '#nav-search-bar-form',
-					'submitButtonSelector' => '#nav-search-submit-button',
-					'formSelectorScript' => '',
-					'submitButtonSelectorScript' => '',
-					'formFields' => array(
-						array(
-							'value' => 'Laptop',
-							'selectorScript' => '#twotabsearchtextbox',
-							'isScript' => false
-						)
-					)
-				),
-				'keepFormdataSelection' => true,
-				'format' => 'html',
-				'submitType' => 'local'
-			);
-			$promisesGetHtml = $QuickScraperClient->getHtml($requestUrl, $parseOptions);
-			
-			// 	$promises = [];
-			// 	for ($index = 0; $index < MockConfig::getConcurrentCount(); $index++) {
-			// 		$promisesGetHtml = $QuickScraperClient->getHtml($requestUrl);
-			// 		$responseArray = json_decode($promisesGetHtml);
-			// 		array_push($promises, $responseArray);
-			// 		$this->assertObjectHasAttribute('data', $responseArray);
-			// 		$this->assertObjectNotHasAttribute('message', $responseArray);
-			// $this->assertNotNull($responseArray);
-			// 	}
-			// 	$this->assertNotEmpty($promises);
+			$promises = [];
+			for ($index = 0; $index < MockConfig::getConcurrentCount(); $index++) {
+				$promisesGetHtml = $QuickScraperClient->getHtml($requestUrl);
+				$responseArray = json_decode($promisesGetHtml);
+				array_push($promises, $responseArray);
+				$this->assertObjectHasAttribute('data', $responseArray);
+				$this->assertObjectNotHasAttribute('message', $responseArray);
+				$this->assertNotNull($responseArray);
+			}
+			$this->assertNotEmpty($promises);
 		} catch (\Exception $error) {
 			$this->assertNotNull($error);
 			$this->expectException($error);
